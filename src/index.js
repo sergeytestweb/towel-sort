200c86fcb4618e3b34db1e05273ae535f68bfd34
Sergey Pozharitsky <pozhar511@gmail.com>
2020-10-02 12:33:21 +0300

    feat: added decision test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+    if (!Array.isArray(matrix)) return [];
+    if (matrix.length < 0) return [];
+    let newArr = [];
+    for (i = 0; i < matrix.length; i++) {
+        if (matrix[i].length > 0) {
+            if (i > 0 && i % 2 !== 0) {
+              matrix[i].reverse();
+              for (j = 0; j < matrix[i].length; j++) {
+                  newArr.push(matrix[i][j]);                }
+            } else {
+                    for (j = 0; j < matrix[i].length; j++) {
+                      newArr.push(matrix[i][j]);
+  
+                }
+            }
+        }
+    }
+    return newArr;
+};
